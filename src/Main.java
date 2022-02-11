dc3d64f676565895babf7a7b89cf2cbfcf7a5fe3
Mark Krysoboy <heter1209@gmail.com>
2022-02-11 09:53:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("01110010", "11010100"));
+        System.out.println(bins.mult("01111111", "11010100"));
+    }
+}
